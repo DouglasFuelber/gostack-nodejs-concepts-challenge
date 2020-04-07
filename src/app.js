31e82166ca89afb99b1ff03b4e6492dff75c6a53
Douglas Fuelber <contato@douglasfuelber.com>
2020-04-06 23:50:48 -0300

    Implemented delete repository route

--- src/app.js
+++ src/app.js
@@ -53,7 +53,16 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0)
+    return res.status(400).json({ error: "Repository with the given id not found" });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
